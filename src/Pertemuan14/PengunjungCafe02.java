63f4415b6d7444e7c9a34dadd9157d3a05bdf429
adisnayottama <adisnayottama1@gmail.com>
2025-11-26 14:23:28 -1200

    Percobaan4

---
+++ src/Pertemuan14/PengunjungCafe02.java
@@ -0,0 +1,15 @@
+package Pertemuan14;
+
+public class PengunjungCafe02 {
+    public static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- "+namaPengunjung[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+    }
+}

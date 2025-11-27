cd95fa31cc8c54e7492566eac5109bfecffb1d26
adisnayottama <adisnayottama1@gmail.com>
2025-11-26 14:41:09 -1200

    Modif2Percob4

--- src/Pertemuan14/PengunjungCafe02.java
+++ src/Pertemuan14/PengunjungCafe02.java
@@ -1,13 +1,16 @@
 package Pertemuan14;
 
 public class PengunjungCafe02 {
-    public static void daftarPengunjung(String...namaPengunjung) {
-        System.out.println("Daftar Nama Pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- "+namaPengunjung[i]);
+    public static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
+
         System.out.println();
     }
+
     public static void main(String[] args) {
         daftarPengunjung("Ali", "Budi", "Citra");
         daftarPengunjung("Andi");

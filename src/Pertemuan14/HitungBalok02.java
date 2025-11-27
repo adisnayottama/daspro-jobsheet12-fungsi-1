f7a372264f65c8086885ebd5375b8978c91d9f35
adisnayottama <adisnayottama1@gmail.com>
2025-11-26 15:43:03 -1200

    ModifPercob5

--- src/Pertemuan14/HitungBalok02.java
+++ src/Pertemuan14/HitungBalok02.java
@@ -2,6 +2,14 @@ package Pertemuan14;
 import java.util.Scanner;
 
 public class HitungBalok02 {
+    static int hitungLuas (int pjg, int lb) {
+        int Luas = pjg * lb;
+        return Luas;
+    }
+    static int hitungVolume(int tinggi, int a, int b) {
+        int Volume = hitungLuas(a, b) * tinggi;
+        return Volume;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
     int p, l, t, L, vol;
@@ -15,11 +23,11 @@ public class HitungBalok02 {
     System.out.println("Masukkan tinggi");
     t = input.nextInt();
 
-    L = p * l;
+    L = hitungLuas(p, l);
     System.out.println("Luas persegi panjang adalah " + L);
-    vol = p * l * t;
+    vol = hitungVolume(t, p, l);
     System.out.println("Volume balok adalah " + vol);
-    
+
     input.close();
     }
 }
